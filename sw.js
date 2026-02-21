9884b72e52d88893cb18adb85a1af6a7439762a8
Worksduvi <125094799+Worksduvi@users.noreply.github.com>
2026-02-21 23:15:04 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "[NOMBRE_APP_SIN_ESPACIOS]-v1"; // Cambia a v2, v3 cuando actualices tu app
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon.png",
+const CACHE_NAME = 'vico-pwa-v6';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
   "https://unpkg.com/lucide@latest",
   "https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&family=Roboto:wght@300;400&display=swap"
 ];
